7c5673e503156650244e3f8db98457f2558810ff
DimaParachute <dimaparachute11@icloud.com>
2022-03-24 12:44:00 +0300

    Initial Commit

---
+++ GalleryTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryTestTask
+//
+//  Created by Дмитрий Фетюхин on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
